018c3fe75a0a3d38f369d0f1032f71a941f56250
Mahmud Ali Baba-kusa <mahmudbkusa@gmail.com>
2025-08-12 17:46:12 +0100

    Clean requirements, env-driven DB, pin Python for Render

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+load_dotenv() 
 
 
 DATABASE_URL = "mysql+mysqlconnector://root:@localhost/taskapp_db"
