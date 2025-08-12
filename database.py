bfbb3cf72604a7d8f763df588af67b3f3e3d0335
Mahmud Ali Baba-kusa <mahmudbkusa@gmail.com>
2025-08-12 23:55:55 +0100

    duudud

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
-#from sqlalchemy import create_engine
-#from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.orm import sessionmaker
-#from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-#load_dotenv() 
+load_dotenv() 
 
 
-#DATABASE_URL = "mysql+mysqlconnector://root:@localhost/taskapp_db"
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost/taskapp_db"
 
-#engine = create_engine(DATABASE_URL)
-#SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
-#Base = declarative_base()
+Base = declarative_base()
 
-# database.py
+
