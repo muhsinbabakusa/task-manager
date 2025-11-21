285be49fe92000623be6e91c590ec54985d66855
Mahmud Ali Baba-kusa <mahmudbkusa@gmail.com>
2025-11-21 23:52:33 +0100

    hh

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-# database.py
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -6,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()  # read .env locally; on Render/Railway their env panel provides vars
 
-DATABASE_URL = "mysql+mysqlconnector://root:uUDXddXFMxVVEAhZWZQlnWdkbFHMPyat@switchyard.proxy.rlwy.net:27561/railway"
+DATABASE_URL = "mysql://root:uUDXddXFMxVVEAhZWZQlnWdkbFHMPyat@switchyard.proxy.rlwy.net:27561/railway"
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
@@ -19,4 +18,18 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./task.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # ✅ remove pool_size/max_overflow
+# )
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
 
